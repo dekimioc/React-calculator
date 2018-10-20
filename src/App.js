6c8305476f3de0b4beb9ed6f64bf8ec4603f9e9c
dekimioc <dekimioc@yahoo.com>
2018-10-20 14:43:27 +0200

    Added handlers and functionality in App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,42 @@ class App extends Component {
     }
   }
 
+  handleClick = e => {
+    const value = e.target.getAttribute('data-value')
+    switch (value) {
+      case 'clear':
+        this.setState({
+          operations: [],
+        })
+        break
+      case 'equal':
+        this.calculateOperations()
+        break
+      default:
+        const newOperations = update(this.state.operations, {
+          $push: [value],
+        })
+        this.setState({
+          operations: newOperations,
+        })
+        break
+    }
+  }
+
+  
+
+  calculateOperations = () => {
+    let result = this.state.operations.join('')
+    if (result) {
+      result = math.eval(result)
+      result = math.format(result, { precision: 14 })
+      result = String(result)
+      this.setState({
+        operations: [result],
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
